c3f0dce49be30a54874ed759b47529d018f78a4e
caai1606 <113484661+caai1606@users.noreply.github.com>
2022-12-13 22:09:16 -0500

    Add files via upload

---
+++ EurekaChristianAraujo/src/test/java/com/example/eurekachristianaraujo/EurekaChristianAraujoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekachristianaraujo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaChristianAraujoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
